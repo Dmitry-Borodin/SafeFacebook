6e7339327ef55299299932312a1536e3186d9eda
Dmitry <mailborodin@gmail.com>
2017-01-22 18:07:56 +0700

    launcher icon added and WebView settings

---
+++ app/src/main/java/com/krenvpravo/safefacebook/CustomWebViewClient.kt
@@ -0,0 +1,32 @@
+package com.krenvpravo.safefacebook
+
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+/**
+ * @author Dmitry Borodin on 2017-01-22.
+ */
+
+class CustomWebViewClient(val callback: WebLoadingCallback) : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        return false //not start browser
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        callback.onUrlLoaded()
+    }
+
+    override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+        super.onReceivedError(view, request, error)
+        callback.onUrlLoadFialed()
+    }
+
+    interface WebLoadingCallback {
+        fun onUrlLoaded()
+        fun onUrlLoadFialed()
+    }
+}
---
+++ app/src/main/java/com/krenvpravo/safefacebook/ui/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.krenvpravo.safefacebook.ui
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.os.Build
+import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.krenvpravo.safefacebook.Constants
+import com.krenvpravo.safefacebook.CustomWebViewClient
+import com.krenvpravo.safefacebook.CustomWebViewClient.WebLoadingCallback
+import com.krenvpravo.safefacebook.R
+
+/**
+ * @author Dmitry Borodin on 2017-01-22.
+ */
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViews()
+    }
+
+    private val webView: WebView by lazy {
+        val webView = WebView(this)
+        webView
+    }
+
+    private fun initViews() {
+        val progressdialog = ProgressDialog(this)
+        progressdialog.setTitle(getString(R.string.progress_dialog_initial_title))
+        webView.setWebViewClient(CustomWebViewClient(object : WebLoadingCallback{
+            override fun onUrlLoaded() {
+                progressdialog.dismiss()
+            }
+
+            override fun onUrlLoadFialed() {
+                reload()
+            }
+        }))
+        setUpWebSettings(webView.settings)
+        setUpCookies(webView)
+        setContentView(webView)
+        webView.loadUrl(Constants.MAIN_URL)
+    }
+
+    private fun reload() {
+        webView.reload()
+    }
+
+
+    private fun setUpWebSettings(settings: WebSettings) {
+        settings.setGeolocationEnabled(false)
+        settings.javaScriptEnabled = false
+        settings.setAppCacheEnabled(true)
+        settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+    }
+
+    private fun setUpCookies(webView: WebView) {
+        val cookieManager = CookieManager.getInstance()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.setAcceptThirdPartyCookies(webView, true)
+        } else {
+            cookieManager.setAcceptCookie(true)
+        }
+    }
+}
